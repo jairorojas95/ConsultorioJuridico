1d953afafab162067236a57145c3972e72547d4d
jairorojas95 <jairojas95@hotmail.com>
2019-02-26 11:55:22 -0500

    version ya casi lista
    esta version ya tiene un reporte de ejemplo

---
+++ ConsultorioJuridico/Views/probador/WebForm1.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/pagina_maestra/paginamaestra.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="juefi2.Views.probador.WebForm1" %>
+
+<%@ Register Assembly="CrystalDecisions.Web, Version=13.0.3500.0, Culture=neutral, PublicKeyToken=692fbea5521e1304" Namespace="CrystalDecisions.Web" TagPrefix="CR" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+   
+      <CR:CrystalReportViewer ID="CrystalReportViewer1" runat="server" AutoDataBind="true" />
+</asp:Content>
---
+++ ConsultorioJuridico/Views/probador/WebForm1.aspx.cs
@@ -0,0 +1,37 @@
+﻿using juefi2.Models.BD;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace juefi2.Views.probador
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            conecMysql conn = new conecMysql();
+
+            //DataSet objdatset = new DataSet();
+
+            DataTable objdattable = new DataTable();
+
+            probador.CrystalReport1 opt = new probador.CrystalReport1();
+
+            objdattable = conn.EjecutarConsulta(" select DISTINCT  usuario.idusuario as id ,concat (usuario.nombre1,' ',usuario.apellido1) as nombre ,"
+                                             + " tipo_proceso.nombre_tipo from proceso as proceso "
+                                             + " inner join usuario on usuario.idusuario = proceso.usuario_proceso "
+                                             + " inner join tipo_proceso on tipo_proceso.idtipo_proceso = proceso.fk_tipoproceso ; ", CommandType.Text);
+
+
+            opt.SetDataSource(objdattable);
+           CrystalReportViewer1.ReportSource = opt;
+           CrystalReportViewer1.Width = 800;
+           CrystalReportViewer1.Height = 800;
+
+        }
+    }
+}
